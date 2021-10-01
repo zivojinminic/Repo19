4eb28e186c1102391504a68c0abd02b5d7d2f7a7
zivojinminic <zivojinminic05@gmail.com>
2021-10-01 20:01:01 +0200

    JSX:JavaScript + XML

---
+++ ex06/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+  render() {
+    const world = "World";
+    return (
+      <div className="App">
+        <h1>Hello, {world}!</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
