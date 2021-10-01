81b325ea41adf75ec88b9c91497e58ac30381a49
zivojinminic <zivojinminic05@gmail.com>
2021-10-01 13:30:15 +0200

    Create React Boilerplate

---
+++ ex03/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, Padawans!</h1>
+      </div>
+    )
+  }
+}
