70a6f08594a03555b8f7f9f5d974291bcbd4f337
zivojinminic <zivojinminic05@gmail.com>
2021-10-01 20:10:27 +0200

    Create React App

--- ex02/src/App.js
+++ ex02/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Padawan!
+          Hello Padawans!
         </p>
         <a
           className="App-link"
